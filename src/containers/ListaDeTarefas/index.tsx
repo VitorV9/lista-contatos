123ba1916218f94f2bfbd551f04d73d9c264e962
VitorVT9 <vsilvab57@gmail.com>
2026-09-14 14:07:03 -0300

    feat: cria tarefas com componentização

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,17 +1,41 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'estudar TS',
+    descricao: 'Ver a aula 3 da Ebac',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Treino de superiores',
+    prioridade: 'urgente',
+    status: 'concluida'
+  },
+  {
+    titulo: 'Pagar a conta de internet',
+    descricao: 'Baixar fatura no Gmail',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo; </p>
     <ul>
-      <li>
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
