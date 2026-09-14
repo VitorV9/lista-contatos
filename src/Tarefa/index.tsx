3c054683f911be30eb39e06f3236eb53ac7b21c4
VitorVT9 <vsilvab57@gmail.com>
2026-09-14 13:19:37 -0300

    feat: adiciona tarefas estilizadas

--- src/Tarefa/index.tsx
+++
@@ -1,3 +0,0 @@
-const Tarefa = () => <div> nome tarefa</div>
-
-export default Tarefa
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,6 +1,17 @@
+import Tarefa from '../../components/Tarefa'
+
 const ListaDeTarefas = () => (
   <main>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo; </p>
+    <ul>
+      <li>
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+      </li>
+    </ul>
   </main>
 )
 
