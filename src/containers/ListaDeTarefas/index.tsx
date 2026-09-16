7ee50fe3992d168feb691950fa7e4fe276c25d82
VitorVT9 <vsilvab57@gmail.com>
2026-09-16 14:22:11 -0300

    feat: estiliza exibição de status e prioridade de tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux'
 
 import Tarefa from '../../components/Tarefa'
-import { Container } from './styles'
+import { Container, Resultado } from './styles'
 
 import { RootReducer } from '../../store'
 
@@ -35,16 +35,26 @@ const ListaDeTarefas = () => {
     }
   }
 
+  const exibeResultadoFiltragem = (quantidade: number) => {
+    let mensagem = ''
+    const complementacao =
+      termo !== undefined && termo.length > 0 ? `e "${termo}"` : ''
+
+    if (criterio === 'todas') {
+      mensagem = `${quantidade} tarefas(s) encontradas(s) como: todas ${complementacao}`
+    } else {
+      mensagem = `${quantidade} tarefas(s) encontradas(s) como: "${`${criterio}=${valor}`}" ${complementacao}`
+    }
+
+    return mensagem
+  }
+
+  const tarefas = filtraTarefas()
+  const mensagem = exibeResultadoFiltragem(tarefas.length)
+
   return (
     <Container>
-      <p>
-        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;{termo}&ldquo;{' '}
-      </p>
-      <ul>
-        <li>{termo}</li>
-        <li>{criterio}</li>
-        <li>{valor}</li>
-      </ul>
+      <Resultado>{mensagem}</Resultado>
       <ul>
         {filtraTarefas().map((t) => (
           <li key={t.id}>
