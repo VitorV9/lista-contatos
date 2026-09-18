42b26be6515e187b12f309d178f48ad0367d0e98
VitorVT9 <vsilvab57@gmail.com>
2026-09-18 14:16:14 -0300

    feat: cria adição de tarefas

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,6 @@
 import styled, { createGlobalStyle } from 'styled-components'
+import variaveis from './variaveis'
+import { Botao } from '../components/Tarefa/styles'
 
 const estiloGlobal = createGlobalStyle`
   * {
@@ -13,6 +15,33 @@ const estiloGlobal = createGlobalStyle`
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 224px auto;
+`
+
+export const MainContainer = styled.main`
+  padding: 0 40px;
+  height: 100vh;
+  overflow-y: scroll;
+`
+
+export const Campo = styled.input`
+  border-radius: 8px;
+  padding: 8px;
+  background-color: #fff;
+  font-weight: bold;
+  color: #666666;
+  border-color: #666666;
+  width: 100%;
+`
+
+export const Titulo = styled.p`
+  display: block;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  font-size: 18px;
+  font-weight: bold;
+`
+export const BotaoSalvar = styled(Botao)`
+  background-color: ${variaveis.verde};
 `
 
 export default estiloGlobal
